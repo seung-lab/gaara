33891631f13fd9623c4b56e6a6a6ace3f29656e6
William Silversmith <william.silversmith@gmail.com>
2026-08-18 16:41:18 -0400

    fix: refactor of pybind11 code needed some work

--- gaara/fastgaara.cpp
+++ gaara/fastgaara.cpp
@@ -13,8 +13,9 @@
 namespace py = pybind11;
 
 template <typename Func>
-py::array dispatch_skeletonize(const py::array& labels, Func&& func) {
+void dispatch_skeletonize(const py::array& labels, Func&& func) {
 	py::dtype dt = labels.dtype();
+	const int width = dt.itemsize();
 
 	const uint64_t sx = labels.shape()[0];
 	const uint64_t sy = labels.shape()[1];
@@ -25,37 +26,43 @@ py::array dispatch_skeletonize(const py::array& labels, Func&& func) {
 	void* data = const_cast<void*>(labels.data());
 
 	if (width == 1) {
-		return func(static_cast<uint8_t*>(data));
+		func(static_cast<uint8_t*>(data), sx, sy, sz);
 	}
 	else if (width == 2) {
-		return func(static_cast<uint16_t*>(data));
+		func(static_cast<uint16_t*>(data), sx, sy, sz);
 	}
 	else if (width == 4) {
-		return func(static_cast<uint32_t*>(data));
+		func(static_cast<uint32_t*>(data), sx, sy, sz);
 	}
 	else {
-		return func(static_cast<uint64_t*>(data));
+		func(static_cast<uint64_t*>(data), sx, sy, sz);
 	}
 }
 
 // assumes fortran order
 py::array thin_palagyi_binary(const py::array& labels) {
-	return dispatch_skeletonize(labels, [](auto *data, uint64_t sx, uint64_t, sy, uint64_t sz) {
-		return gaara::binary::skeletonize<decltype(data)>(
-			static_cast<data_type*>(const_cast<void*>(labels.data())),
+	dispatch_skeletonize(labels, [](auto *data, uint64_t sx, uint64_t sy, uint64_t sz) {
+		using T = std::remove_pointer_t<decltype(data)>;
+		return gaara::binary::skeletonize<T>(
+			data,
 			sx, sy, sz
 		);
 	});
+
+	return labels;
 }
 
 // assumes fortran order
 py::array thin_palagyi_multilabel(const py::array& labels) {
-	return dispatch_skeletonize(labels, [](auto *data, uint64_t sx, uint64_t, sy, uint64_t sz) {
-		return gaara::multilabel::skeletonize<decltype(data)>(
-			static_cast<data_type*>(const_cast<void*>(labels.data())),
+	dispatch_skeletonize(labels, [](auto *data, uint64_t sx, uint64_t sy, uint64_t sz) {
+		using T = std::remove_pointer_t<decltype(data)>;
+		return gaara::multilabel::skeletonize<T>(
+			data,
 			sx, sy, sz
 		);
 	});
+
+	return labels;
 }
 
 PYBIND11_MODULE(fastgaara, m) {
