2e82eea57ea2d5e67eb6a5c1d124dd1b4de0da56
William Silversmith <william.silversmith@gmail.com>
2026-08-20 17:33:58 -0400

    feat: two functions for thinning vs full skeletonization

--- gaara/__init__.py
+++ gaara/__init__.py
@@ -5,9 +5,9 @@ import osteoid
 
 from . import fastgaara
 
-__all__ = ["skeletonize"]
+__all__ = ["thin", "skeletonize"]
 
-def skeletonize(
+def thin(
 	labels:npt.NDArray[np.integer],
 	binary_image:bool = False,
 	in_place:bool = False,
@@ -32,7 +32,9 @@ def skeletonize(
 	if labels.size <= 1:
 		return labels
 
-	if in_place and not labels.flags.f_contiguous:
+	if in_place and not labels.flags.writeable:
+		raise ValueError("Cannot perform an in-place operation on non-writeable data.")
+	elif in_place and not labels.flags.f_contiguous:
 		raise ValueError("Cannot perform an in-place operation on non-Fortran ordered data.")
 	elif not in_place:
 		labels = np.copy(labels, order="F")
@@ -43,10 +45,55 @@ def skeletonize(
 		labels = labels.view(np.uint8)
 
 	if binary_image:
-		(vertices, edges) = fastgaara.thin_palagyi_binary(labels, preserve_endpoints)
+		fastgaara.thin_binary(labels, preserve_endpoints)
+	else:
+		fastgaara.thin_multilabel(labels, preserve_endpoints)
+
+	return labels
+
+def skeletonize(
+	labels:npt.NDArray[np.integer],
+	binary_image:bool = False,
+	in_place:bool = False,
+	preserve_endpoints:bool = False,
+) -> tuple[osteoid.Skeleton|dict[int,osteoid.Skeleton], npt.NDArray[np.integer]]:
+	"""
+	Apply Palagyi's 3D voxel thinning algorithm to `labels`, a binary image
+	and return skeletons.
+
+	Reference:
+
+	K. Palágyi, "A Sequential 3D Curve-Thinning Algorithm Based on Isthmuses,"
+	in Advances in Visual Computing, vol. 8888,
+	G. Bebis, R. Boyle, B. Parvin, D. Koracin, R. McMahan, J. Jerald, 
+	H. Zhang, S. M. Drucker, C. Kambhamettu, M. El Choubassi, Z. Deng, 
+	and M. Carlson, Eds., 
+	Cham: Springer International Publishing, 2014, pp. 406–415.
+	doi: 10.1007/978-3-319-14364-4_39.
+	"""
+	if labels.ndim != 3:
+		raise ValueError(f"This function only supports 3D images. Got: {labels.shape}")
+
+	if labels.size <= 1:
+		return labels
+
+	if in_place and not labels.flags.writeable:
+		raise ValueError("Cannot perform an in-place operation on non-writeable data.")
+	elif in_place and not labels.flags.f_contiguous:
+		raise ValueError("Cannot perform an in-place operation on non-Fortran ordered data.")
+	elif not in_place:
+		labels = np.copy(labels, order="F")
+
+	orig_dtype = labels.dtype
+	if labels.dtype == bool:
+		binary_image = True
+		labels = labels.view(np.uint8)
+
+	if binary_image:
+		(vertices, edges) = fastgaara.skeletonize_binary(labels, preserve_endpoints)
 		skeletons = osteoid.Skeleton(vertices, edges)
 	else:
-		skeletons = fastgaara.thin_palagyi_multilabel(labels, preserve_endpoints)
+		skeletons = fastgaara.skeletonize_multilabel(labels, preserve_endpoints)
 		skeletons = { 
 			segid: osteoid.Skeleton(vertices, edges) 
 			for segid, (vertices, edges) in skeletons.items()
