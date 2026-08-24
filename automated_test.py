ef5873c03211cf378cedcbafecd404702ff781ab
William Silversmith <william.silversmith@gmail.com>
2026-08-24 14:52:59 -0400

    release(0.5.0): adds anchor parameter

--- automated_test.py
+++ automated_test.py
@@ -4,12 +4,12 @@ import pytest
 
 @pytest.mark.parametrize("binary_image", [True, False])
 def test_thin(binary_image):
-	ones = np.ones([100,100,100], dtype=np.uint64, order="F")
+	ones = np.ones([3,3,3], dtype=np.uint64, order="F")
 	ones[0,0,0] = 0
 	
 	gaara.thin(ones, in_place=False, binary_image=binary_image)
 
-	assert np.sum(ones) == 100 * 100 * 100 - 1
+	assert np.sum(ones) == 3 * 3 * 3 - 1
 
 	gaara.thin(ones, in_place=True, binary_image=binary_image)
 
