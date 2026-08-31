6668743a03901a5459d7ba7b8352612c0cfada7c
William Silversmith <william.silversmith@gmail.com>
2026-08-31 13:52:49 -0400

    test: check extraction

--- automated_test.py
+++ automated_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gaara
 import pytest
+import crackle
 
 @pytest.mark.parametrize("binary_image", [True, False])
 def test_thin(binary_image):
@@ -14,3 +15,32 @@ def test_thin(binary_image):
 	gaara.thin(ones, in_place=True, binary_image=binary_image)
 
 	assert np.sum(ones) == 1
+
+
+def test_extract_skeleton_crackle():
+	sz = 200
+	img = np.zeros([2000,2000,200], dtype=np.uint64, order="F")
+	img[5,5,:] = 1
+	img[9,9,:] = 2
+
+	img = crackle.compressa(img)
+	skels = gaara.extract_skeletons_crackle(
+		img,
+		
+	)
+
+	skel = skels[1]
+	assert np.all(skel.vertices[:,0] == 5)
+	assert np.all(skel.vertices[:,1] == 5)
+	assert np.all(skel.vertices[:,2] == np.arange(200))
+	
+
+	skel = skels[2]
+	assert np.all(skel.vertices[:,0] == 9)
+	assert np.all(skel.vertices[:,1] == 9)
+	assert np.all(skel.vertices[:,2] == np.arange(200))
+	
+
+
+
+
